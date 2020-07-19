4016b8b76247051812bb5e2205a01eadea431e94
LIJIANJUN <matwetu@gmail.com>
2020-07-19 23:03:37 +0800

    update jdwp-lib-injector.sh

--- frida/jdwp-lib-injector.sh
+++ frida/jdwp-lib-injector.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# use with: git@github.com:ikoz/jdwp-lib-injector.git
+# used with: git@github.com:ikoz/jdwp-lib-injector.git
 # usage: ./script dso package_name
-
+set -x
 if ! test -d jdwp-lib-injector; then
   git clone git@github.com:ikoz/jdwp-lib-injector.git
 fi
 
 echo "[**] Android JDWP library injector by @ikoz"
-if [ ! -f "$1" ]
-  then
-    echo "[!!] Please provide the path to a valid library SO file as the first argument"
-    exit 1
+if [ ! -f "$1" ]; then
+  echo "[!!] Please provide the path to a valid library SO file as the first argument"
+  exit 1
+fi
+if ! adb shell pm path "$2" > /dev/null; then
+  echo "[!!] Please provide a valid package name as the second argument"
+  exit 1
 fi
 adb shell am clear-debug-app
 adb shell am set-debug-app -w --persistent $2
 adb shell am force-stop $2
 adb logcat -c
-adb shell monkey -p $2 -c android.intent.category.LAUNCHER --wait-dbg 1
+adb shell monkey -p $2 -c android.intent.category.LAUNCHER --wait-dbg 1 2>&1 | sed "s/^/\[**\] monkey says: /"
 #adb shell am start -D -n $2/$2.MainActivity
 #exit 0
 adb shell rm /data/local/tmp/`basename $1` -f # don't add -r here
@@ -42,7 +45,7 @@ fi
 echo "[**] Will forward tcp:8700 to jdwp:$jdwp_pid"
 adb forward tcp:8700 jdwp:$jdwp_pid
 echo "[**] Starting jdwp-shellifier.py to load library"
-python jdwp-lib-injector/jdwp-shellifier.py --target 127.0.0.1 --port 8700 --break-on android.app.Activity.onCreate --loadlib `basename $1`
+python jdwp-lib-injector/jdwp-shellifier.py --target 127.0.0.1 --port 8700 --break-on android.app.Application.onCreate --loadlib `basename $1`
 echo "[**] Running frida-ps -U. If you see 'Gadget' then all worked fine!"
 #frida-ps -U
 timeout 5 adb logcat *:S tpmm:V
