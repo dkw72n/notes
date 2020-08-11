995218a80f5865a8cf3eb01cb6afa000f109796f
dkw72n <matwetu+ad@gmail.com>
2020-08-11 22:44:59 +0800

    commit

--- mobiledevice/xcode_mitm.py
+++ mobiledevice/xcode_mitm.py
@@ -5,7 +5,11 @@ import struct
 from collections import namedtuple
 import itertools
 import time
-
+from ctypes import sizeof
+from dtxlib import DTXMessage, DTXMessageHeader,    \
+    auxiliary_to_pyobject, pyobject_to_auxiliary,   \
+    pyobject_to_selector, selector_to_pyobject
+    
 COUNTER = itertools.count()
 
 from ctypes import c_uint32, c_uint16, Structure
@@ -24,6 +28,20 @@ def hexdump(buf):
   for l in lines:
     print(form1(l), form2(l))
 
+def hexdump2(buf, prefix=''):
+  if not buf: return prefix + '<None>\n'
+  allowed = '_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890+-_=()*&^%$#@![]{}\\|;\':"<>?,./`~'
+  def form1(b):
+    return ' '.join(map(lambda x:'%02x' % x, b))
+  def form2(b):
+    return ''.join(map(lambda c: c if c in allowed else '.', b.decode('ascii', "replace")))
+  LINE_BYTES = 32
+  lines = [buf[i: i + LINE_BYTES] for i in range(0, len(buf), LINE_BYTES)]
+  ret = []
+  for l in lines:
+    ret += [prefix, form1(l), ' ', form2(l), '\n']
+  return ''.join(ret)
+  
 class UsbmuxHeader(Structure):
   _fields_ = [
     ('length', c_uint32),
@@ -226,9 +244,149 @@ class TLSDecoder:
     
     self._reset()
 
-class DTXDecoder:
-  pass
+
+class RawArrayDecoder:
   
+  def __init__(self, expected):
+    self.expected = expected
+    self._buf = []
+    print("RawArrayDecoder ", expected)
+
+  def update(self, buf):
+    to_consume = min(len(buf), self.expected)
+    if to_consume:
+      self.expected -= to_consume
+      self._buf.append(buf[:to_consume])
+    if self.expected == 0:
+      return None, to_consume
+    return self, to_consume
+  
+  @property
+  def data(self):
+    return b''.join(self._buf)
+
+class DTXHeaderDecoder:
+
+  def __init__(self):
+    self._d = RawArrayDecoder(sizeof(DTXMessageHeader))
+    self._hdr = None
+    
+  def update(self, buf):
+    d, p = self._d, 0
+    while d is not None and p < len(buf):
+      d, p_ = self._d.update(buf[p:])
+      p += p_
+    
+    if not d:
+      self._hdr = DTXMessageHeader.from_buffer_copy(self._d.data)
+      return None, p
+    return self, p
+  
+  @property
+  def data(self):
+    return self._hdr
+
+ 
+
+class FragmentDecoder:
+  
+  def __init__(self):
+    self._d = DTXHeaderDecoder()
+    self._hdr = None
+    self._body = None
+    
+  def update(self, buf):
+    d, p = self._d, 0
+    while d is not None and p < len(buf):
+      d, p_ = self._d.update(buf[p:])
+      p += p_
+      
+    if not d:
+      if not self._hdr:
+        self._hdr = self._d.data
+        if self._hdr.fragmentCount > 1 and self._hdr.fragmentId == 0:
+          return None, p
+        else:
+          self._d = RawArrayDecoder(self._hdr.length)
+          return self, p
+      else:
+        self._body = self._d.data
+        return None, p
+    return self, p
+  
+  @property
+  def data(self):
+    return (self._hdr, self._body)
+    """
+    if self._hdr.fragmentCount == 1 and self._hdr.fragmentId == 0:
+      try:
+        dtx = DTXMessage.from_bytes(bytes(self._hdr) + self._body)
+        sel = dtx.get_selector()
+        if sel.startswith(b'bplist00'):
+          sel = selector_to_pyobject(sel)
+        c = dtx.get_auxiliary_count()
+        return (sel, c, [auxiliary_to_pyobject(dtx.get_auxiliary_at(i)) for i in range(c)])
+      except:
+        return f"<decode error>\n " + hexdump2(self._body)
+    return (self._hdr.magic, f'{self._hdr.fragmentId}/{self._hdr.fragmentCount}', f'{self._hdr.channelCode}-{self._hdr.identifier}', hexdump2(self._body))
+    """
+    
+class DTXDecoder:
+  
+  def __init__(self, tag, conn):
+    self.tag = tag
+    self.conn = conn
+    self._d = FragmentDecoder()
+    self._fp = open(f"dtx-{id(self)}.log", "w")
+    self._fp_bin = open(f"dtx-{id(self)}.log.bin", "wb")
+    self._fragment_tracker = {}
+    
+  def update(self, buf):
+    self._fp_bin.write(buf)
+    d = self._d
+    while buf:
+      d, p = self._d.update(buf)
+      if d == None:
+        # print(self._d.data)
+        # self._fp.write(f'{self._d.data}\n')
+        # self.log(*self._d.data)
+        self._d = FragmentDecoder()
+      buf = buf[p:]
+  
+  def log_dtx_msg(self, buf):
+    try:
+      dtx = DTXMessage.from_bytes(buf)
+      sel = dtx.get_selector()
+      if sel.startswith(b'bplist00'):
+        sel = selector_to_pyobject(sel)
+        c = dtx.get_auxiliary_count()
+      self._fp.write(f"\tsel={sel}\n")
+      if c:
+        for i in range(c):
+          self._fp.write(f"\t\targ{i}={auxiliary_to_pyobject(dtx.get_auxiliary_at(i))}\n")
+    except:
+      self._fp.write(f"\t! decode error !\n")
+      self._fp.write(hexdump2(buf, '\t'))
+
+  def log(self, hdr, body):
+    k = f'<{hdr.channelCode}-{hdr.identifier}>'
+    self._fp.write(f"<{hdr.channelCode}-{hdr.identifier}> <{hdr.fragmentId}/{hdr.fragmentCount}> magic={hdr.magic}\n")
+    if hdr.fragmentCount == 1 and hdr.fragmentId == 0:
+      self.log_dtx_msg(bytes(hdr) + body)
+    else:
+      if hdr.fragmentCount > 1:
+        if hdr.fragmentId == 0:
+          self._fragment_tracker[k] = [bytes(hdr)]
+        else:
+          self._fragment_tracker[k] += [bytes(hdr), body]
+        if hdr.fragmentId == hdr.fragmentCount - 1:
+          self.log_dtx_msg(b''.join(self._fragment_tracker[k]))
+          self._fragment_tracker.pop(k)
+      else:
+        assert False, "WTF?"
+      #self._fp.write(hexdump2(body, '\t'))
+    
+
 class LockdownDecoder:
   
   def __init__(self, tag, conn):
@@ -303,6 +461,10 @@ class LockdownDecoder:
       msg = self.hdr + self.body
       if msg[:3] == b'\x16\x03\x01' or msg[:3] == b'\x16\x03\x03':
         self.hijack = TLSDecoder(self.tag, self.conn)
+        self.hijack.update(msg)
+        self._reset()
+      elif msg[:4] == b'\x79\x5b\x3d\x1f':
+        self.hijack = DTXDecoder(self.tag, self.conn)
         self.hijack.update(msg)
         self._reset()
       else:
