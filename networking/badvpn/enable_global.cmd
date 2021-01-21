f2e9e8b86d675e010ed6311337c6d19e46eac1f8
dkw72n <matwetu+ad@gmail.com>
2021-01-21 10:58:34 +0800

    simplified

--- networking/badvpn/enable_global.cmd
+++ networking/badvpn/enable_global.cmd
@@ -1,10 +1,6 @@
-
 route add 10.0.0.0/8 10.11.128.1 metric 5
-route delete 0.0.0.0
+route add 8.8.8.8/32 192.168.222.1 metric 5
+route add 8.8.4.4/32 192.168.222.1 metric 5
 route add 0.0.0.0/0 192.168.222.1 metric 5
 
-pause
-
-route delete 0.0.0.0
-route add 0.0.0.0/0 10.11.128.1 metric 5
-route delete 10.0.0.0
+ping 192.168.222.1
